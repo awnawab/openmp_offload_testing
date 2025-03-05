43a00902e06ced486fba7faf5c354e3ac2c78427
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-03-05 20:36:06 +0000

    Remove OpenACC linking

--- cmake/compile_options.cmake
+++ cmake/compile_options.cmake
@@ -1,3 +1,2 @@
-set( OpenMP_Fortran_FLAGS   "-mp=bind,allcores,numa" CACHE STRING "" )
-set( OpenACC_Fortran_FLAGS "-acc=gpu -mp=gpu -gpu=cc80 -Minfo=mp" CACHE STRING "" )
+set( OpenMP_Fortran_FLAGS   "-mp=gpu -mp=bind,allcores,numa  -gpu=cc80 -Minfo=mp" CACHE STRING "" )
 set( CMAKE_Fortran_FLAGS "-O2 -gopt -fpic" )
